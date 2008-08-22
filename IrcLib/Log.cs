7a33238b0e03d460609b68f5664b91b8ac8afe74
Erik <Erik@18eca4a0-bae6-7843-a9c8-d3f318ed89ea>
2008-08-22 02:20:49 +0000

    Added logging functionality
    git-svn-id: svn://saturn/Confabulation/trunk@13 18eca4a0-bae6-7843-a9c8-d3f318ed89ea

---
+++ IrcLib/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Confabulation.Chat
+{
+	public static class Log
+	{
+		public static void WriteLine(string text)
+		{
+			TextWriter syncWriter = TextWriter.Synchronized(writer);
+			syncWriter.WriteLine(text);
+			syncWriter.Flush();
+		}
+
+		private static StreamWriter writer = new StreamWriter("Confabulation.log");
+	}
+}
